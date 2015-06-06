f6b421768baae273a51392d6a4389d6f4a087d62
pvelchev <pbx@developer.bg>
2015-06-06 09:01:51 +0300

    www

--- www/js/map.js
+++ www/js/map.js
@@ -1,4 +1,4 @@
-// $(window).bind("load", onDeviceReady);
+//$(window).bind("load", onDeviceReady);
 document.addEventListener("deviceready", onDeviceReady, false);
 
 $('#map').bind('pageshow',function(event, ui){ getGeolocation();} );
@@ -11,16 +11,12 @@ function onDeviceReady() {
 			e.preventDefault();
 		}
 		else {
-			if (confirm("Are you sure you want to exit?")) {
-				navigator.app.exitApp();
-			}
-			else {
-				return false;
-			}
+			exitApp()
 		}
-        }, 
-		false );
-		onMapLoad();
+
+	},
+	false );
+	onMapLoad();
 }
 
 
@@ -67,6 +63,73 @@ function geoError(error) {
 }
 
 function exitApp(){
-	if (confirm("Are you sure you want to exit?"))
-	navigator.app.exitApp();
+	var act_page = '#'+$.mobile.activePage.attr('id')+' ';
+	var active = $(act_page+'.ui-btn-active');
+	$(active).removeClass('ui-btn-active');
+	$( act_page +'#exit_btn').addClass('ui-btn-active');
+	if (confirm("Are you sure you want to exit?")) navigator.app.exitApp();
+	
+	$(act_page+'#exit_btn').removeClass('ui-btn-active');
+	$(active).addClass('ui-btn-active');
+	
 }
+
+/***********************
+.ajax_loader {background: url("spinner_squares_circle.gif") no-repeat center center transparent;width:100%;height:100%;}
+.blue-loader .ajax_loader {background: url("ajax-loader_blue.gif") no-repeat center center transparent;}
+
+var box2;
+$(".box-2").live('click', function(){	
+		box2 = new ajaxLoader(this, {classOveride: 'blue-loader'});
+	});
+	
+box2.remove()
+***********************/
+
+
+function ajaxLoader (el, options) {
+	// Becomes this.options
+	var defaults = {
+		bgColor 		: '#fff',
+		duration		: 800,
+		opacity			: 0.7,
+		classOveride 	: false
+	}
+	this.options 	= jQuery.extend(defaults, options);
+	this.container 	= $(el);
+	
+	this.init = function() {
+		var container = this.container;
+		// Delete any other loaders
+		this.remove(); 
+		// Create the overlay 
+		var overlay = $('<div></div>').css({
+				'background-color': this.options.bgColor,
+				'opacity':this.options.opacity,
+				'width':container.width(),
+				'height':container.height(),
+				'position':'absolute',
+				'top':'0px',
+				'left':'0px',
+				'z-index':99999
+		}).addClass('ajax_overlay');
+		// add an overiding class name to set new loader style 
+		if (this.options.classOveride) {
+			overlay.addClass(this.options.classOveride);
+		}
+		// insert overlay and loader into DOM 
+		container.append(
+			overlay.append(
+				$('<div></div>').addClass('ajax_loader')
+			).fadeIn(this.options.duration)
+		);
+    };
+	
+	this.remove = function(){
+		var overlay = this.container.children(".ajax_overlay");
+		if (overlay.length) {
+			overlay.fadeOut(this.options.classOveride, function() {
+				overlay.remove();
+			});
+		}	
+	}
