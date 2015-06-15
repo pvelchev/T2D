2d3e6697b43aed83d8061452e6ec89859ddf47cc
pvelchev <pbx@developer.bg>
2015-06-15 06:11:16 +0300

    ss

--- www/js/script.js
+++ www/js/script.js
@@ -1,19 +1,9 @@
+var _i_=0;
 function startTime() {
-    var today=new Date();
-    var h=today.getHours();
-    var m=today.getMinutes();
-    var s=today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
-    var t = setTimeout(function(){startTime()},500);
+	_i_++;
+    document.getElementById('clock').innerHTML = _i_;
+    var t = setTimeout(function(){startTime()},1000);
 }
-
-function checkTime(i) {
-    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
 
 
 var isConnected = false;
